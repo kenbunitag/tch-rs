cb2f9721c91bf28a55f290081b347db1fe9ef873
Michael Scheuner <michael.scheuner@kenbun.de>
2023-08-21 16:21:37 +0200

    set mobile as default-feature

--- build.sh
+++ build.sh
@@ -11,18 +11,18 @@ set -eu
 #-x     The  shell shall write to standard error a trace for each command after it expands the command and
 #       before it executes it. It is unspecified whether the command that turns tracing off is traced.
 
-LIBTORCH_VERSION=2.0.1
+#LIBTORCH_VERSION=2.0.1
 
-if [ -f "libtorch-cxx11-abi-shared-with-deps-${LIBTORCH_VERSION}+cpu.zip" ]; then
-    echo "Skipping libtorch download."
-else
-  wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-${LIBTORCH_VERSION}%2Bcpu.zip
-fi
-unzip -o libtorch-cxx11-abi-shared-with-deps-${LIBTORCH_VERSION}+cpu.zip
+#if [ -f "libtorch-cxx11-abi-shared-with-deps-${LIBTORCH_VERSION}+cpu.zip" ]; then
+#    echo "Skipping libtorch download."
+#else
+#  wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-${LIBTORCH_VERSION}%2Bcpu.zip
+#fi
+#unzip -o libtorch-cxx11-abi-shared-with-deps-${LIBTORCH_VERSION}+cpu.zip
 
-export LIBTORCH=$(pwd)/libtorch/
-export LD_LIBRARY_PATH=$(pwd)/libtorch/lib/
-ls $LD_LIBRARY_PATH
+#export LIBTORCH=$(pwd)/libtorch/
+#export LD_LIBRARY_PATH=$(pwd)/libtorch/lib/
+#ls $LD_LIBRARY_PATH
 
 cargo fmt --all -- --check \
 && cargo clippy --all --all-features -- -D warnings \
